799f461bbab0266c89f421e83ab381d9ca715101
jucamargo7 <juanpablo9911@hotmail.com>
2023-05-11 23:13:19 -0500

    Agregando pruebas unitarias

--- src/app/core/login/login.component.spec.ts
+++ src/app/core/login/login.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +11,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
     })
     .compileComponents();
 
@@ -20,4 +24,30 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+
+  it("Debe retornar formulario invalido", () =>{
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const email = component.loginForm.controls["email"]
+    email.setValue("juanpablo9911hotmail.com")
+    expect(component.loginForm.invalid).toBeTruthy();
+  });
+
+  it("Debe retornar formulario valido", () =>{
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const email = component.loginForm.controls["email"]
+    const password = component.loginForm.controls["password"]
+
+    email.setValue("juanpablo9911@hotmail.com")
+    password.setValue("12345")
+
+    expect(component.loginForm.invalid).toBeFalse();
+  });
+
+})
